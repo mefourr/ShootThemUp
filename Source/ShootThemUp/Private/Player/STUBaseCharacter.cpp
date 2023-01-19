406f82c43dd504a021fc1a644a4d5e98873aa3df
mefourr <mitchelll8@mail.ru>
2023-01-19 12:46:51 +0300

    Jump System

--- Source/ShootThemUp/Private/Player/STUBaseCharacter.cpp
+++ Source/ShootThemUp/Private/Player/STUBaseCharacter.cpp
@@ -36,6 +36,7 @@ void ASTUBaseCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCo
     PlayerInputComponent->BindAxis("MoveRight", this, &ASTUBaseCharacter::MoveRight);
     PlayerInputComponent->BindAxis("LookUp", this, &ASTUBaseCharacter::AddControllerPitchInput);
     PlayerInputComponent->BindAxis("TurnAround", this, &ASTUBaseCharacter::AddControllerYawInput);
+    PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ASTUBaseCharacter::Jump);
 }
 
 void ASTUBaseCharacter::MoveForward(float Amount)
