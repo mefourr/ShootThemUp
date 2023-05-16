810eb329f437fabe9b9a7eadca4022947df8d1c5
mefourr <mitchelll8@mail.ru>
2023-05-16 05:46:11 +0300

    add new STUPlayerCharacter class

---
+++ Source/ShootThemUp/Public/Player/STUPlayerCharacter.h
@@ -0,0 +1,17 @@
+// Shoot Them Uo Game. All Right Reserved 
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Player/STUBaseCharacter.h"
+#include "STUPlayerCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTTHEMUP_API ASTUPlayerCharacter : public ASTUBaseCharacter
+{
+	GENERATED_BODY()
+	
+};
